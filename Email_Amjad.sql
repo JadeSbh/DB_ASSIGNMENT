795556ceddff3abba9e45b2e178dd83a94540344
JadeSbh <jadework777@gmail.com>
2021-05-05 22:05:55 -0400

    final temple

--- Email_Amjad.sql
+++ Email_Amjad.sql
@@ -2,6 +2,15 @@
 
 /* according to my interruption of the text, there are 8 entities and one many_to_many
 relationship. Therefore, I have 9 tables to create   */
+CREATE TABLE users
+(
+    user_email    Text primary key,
+    user_name     VARCHAR(50),
+    user_password CHAR(60)
+);
+INSERT INTO users
+VALUES ('jade@gmail.com', 'jade', 'hay');
+--DROP TABLE users;
 
 CREATE TABLE box
 (
@@ -41,43 +50,46 @@ VALUES ('Internet class');
 
 CREATE TABLE contact
 (
-
     contact_id            INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     contact_name          TEXT,
     contact_phone         VARCHAR(25),
     contact_email_address TEXT,
+    user_email            text REFERENCES users (user_email) NOT NULL,
     folder_id             INTEGER REFERENCES folder (folder_id) NOT NULL
 
 );
-INSERT INTO contact(contact_name, contact_phone, contact_email_address, folder_id)
-VALUES ('Alice', '555-666-0000', 'Alice@gmail.com', 1);
-INSERT INTO contact(contact_name, contact_phone, contact_email_address, folder_id)
-VALUES ('John', '111-222-0000', 'john@gmail.com', 1);
-INSERT INTO contact(contact_name, contact_phone, contact_email_address, folder_id)
-VALUES ('Lara', '777-777-7777', 'Lara@gmail.com', 1);
-INSERT INTO contact(contact_name, contact_phone, contact_email_address, folder_id)
-VALUES ('Rana', '777-222-9999', 'Rana@gmail.com', 1);
+INSERT INTO contact(contact_name, contact_phone, contact_email_address, user_email, folder_id)
+VALUES ('Alice', '555-666-0000', 'Alice@gmail.com', 'jade@gmail.com', 1);
+INSERT INTO contact(contact_name, contact_phone, contact_email_address, user_email, folder_id)
+VALUES ('John', '111-222-0000', 'john@gmail.com', 'jade@gmail.com', 1);
+INSERT INTO contact(contact_name, contact_phone, contact_email_address, user_email, folder_id)
+VALUES ('Lara', '777-777-7777', 'Lara@gmail.com', 'jade@gmail.com', 1);
+INSERT INTO contact(contact_name, contact_phone, contact_email_address, user_email, folder_id)
+VALUES ('Rana', '777-222-9999', 'Rana@gmail.com', 'jade@gmail.com', 1);
 
 
 --DROP TABLE contact cascade;
+
 CREATE TABLE email
 (
     email_id      INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     email_date    TIMESTAMP DEFAULT now(),
     email_subject TEXT,
     email_content TEXT,
-    contact_id    INTEGER REFERENCES contact (contact_id)
+    contact_id    INTEGER REFERENCES contact (contact_id),
+    user_email    text REFERENCES users (user_email)
 );
-INSERT INTO email(email_date, email_subject, email_content, contact_id)
-VALUES ('2021-05-05', 'Database exercises', 'HI, its Alice, Did you enjoy the exercises with Mr.Denis', 1);
-INSERT INTO email(email_date, email_subject, email_content, contact_id)
-VALUES ('2021-05-05', 'Programing modification assignment', 'Hi its John, How do you find the assignment?', 2);
+INSERT INTO email(email_date, email_subject, email_content, contact_id,user_email)
+VALUES ('2021-05-05', 'Database exercises', 'HI, its Alice, Did you enjoy the exercises with Mr.Denis', 1,'jade@gmail.com' );
+INSERT INTO email(email_date, email_subject, email_content, contact_id,user_email)
+VALUES ('2021-05-05', 'Programing modification assignment', 'Hi its John, How do you find the assignment?', 2,'jade@gmail.com');
 --DROP TABLE email cascade ;
 CREATE TABLE conversation
 (
     conversation_id   INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     conversation_date TIMESTAMP DEFAULT now(),
-    email_id          INTEGER REFERENCES email (email_id)
+    contact_id        INTEGER REFERENCES contact (contact_id),
+    email_id          INTEGER REFERENCES email (email_id) NOT NULL
 );
 INSERT INTO conversation(conversation_date, email_id)
 VALUES ('2021-05-05', 1);
@@ -91,7 +103,7 @@ VALUES ('2021-05-05', 2);
    constrained of (conversation-id, contact-id)
  */
 
-
+/*
 CREATE TABLE member
 (
     member_id       INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -102,31 +114,47 @@ CREATE TABLE member
    contact Alice initiated with her email that its email-id is 1
  */
 INSERT INTO member(contact_id, conversation_id)
-INSERT VALUES ( 3, 1);
+VALUES (3, 1);
 INSERT INTO member(contact_id, conversation_id)
 VALUES (4, 1);
+SELECT *
+FROM member;
 --DROP TABLE member cascade;
 /* little note: as before this one could be represented as a weak
    entity in ER relational model.*/
-
-CREATE TABLE replay
+*/
+CREATE TABLE reply
 (
-    replay_id      INTEGER GENERATED ALWAYS AS IDENTITY,
-    replay_content TEXT,
-    member_id      INTEGER REFERENCES member (member_id) NOT NULL,
-    primary key (replay_id, member_id)
-);
-INSERT INTO replay(replay_content, member_id)
-VALUES ('HI Alice, yes I do enjoy his database class', 1);
-INSERT INTO replay(replay_content, member_id)
-VALUES ('HI Alice, its Rana, yes I did enjoy it, you?', 2);
+    reply_id       INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    conversation_id INTEGER REFERENCES conversation (conversation_id) NOT NULL,
+    reply_content  TEXT,
+    parent_id       INTEGER REFERENCES reply(reply_id)
 
-CREATE TABLE email_label
+);
+INSERT INTO reply(conversation_id,parent_id, reply_content)
+VALUES (1, NULL, 'HI Alice, its Lara, yes I do enjoy his database class');
+INSERT INTO reply(conversation_id, parent_id, reply_content)
+VALUES (1, 1, 'HI Alice, its Rana, yes I did enjoy it, you?');
+SELECT *
+FROM reply;
+CREATE TABLE contact_label
 (
     contact_id INTEGER REFERENCES contact (contact_id) NOT NULL,
     label_id   INTEGER REFERENCES label (label_id)     NOT NULL,
     primary key (contact_id, label_id)
 );
 
---Drop TABLE replay cascade;
+Drop TABLE reply cascade;
+CREATE TABLE contact_conversation
+(
+    contact_id      INTEGER REFERENCES contact (contact_id),
+    conversation_id INTEGER REFERENCES conversation (conversation_id),
+    primary key (contact_id, conversation_id)
 
+);
+CREATE TABLE contact_email
+(
+    contact_id INTEGER REFERENCES contact (contact_id),
+    email_id   INTEGER REFERENCES email (email_id),
+    primary key (contact_id, email_id)
+)
